9d611aebeb3c4a07cd080cb8f0b8998a89331569
rstiskalek <richard.stiskalek@protonmail.com>
2023-10-24 09:32:22 +0100

    Update script

--- tngsorted/select_box.py
+++ tngsorted/select_box.py
@@ -15,15 +15,9 @@
 """
 Script to select particles within a sub-box of a simulation snapshot.
 """
-from datetime import datetime
-from gc import collect
-
 import MAS_library as MASL
 import numpy
-from h5py import File
 from numba import jit
-
-from tngsorted import get_snapshot_files
 
 
 @jit(nopython=True, fastmath=True, boundscheck=False)
@@ -48,10 +42,25 @@ def find_next_particle(start_index, end_index, pos, x0, y0, z0,
     return None
 
 
-def find_boxed_from_particles(pos, center, subbox_size, boxsize):
+def find_boxed(pos, center, subbox_size, boxsize):
     """
-    Find all indices of particles in a box of size `subbox_size` centered on
+    Find positions of particles in a box of size `subbox_size` centered on
     `center`, where the simulation box size is `boxsize`.
+
+    Parameters
+    ----------
+    pos : 2-dimensional array of shape (nsamples, 3)
+        Positions of all particles in the simulation.
+    center : 1-dimensional array
+        Center of the sub-box.
+    subbox_size : float
+        Size of the sub-box.
+    boxsize : float
+        Size of the simulation box.
+
+    Returns
+    -------
+    pos : 2-dimensional array of shape (nsubsamples, 3)
     """
     if isinstance(center, list):
         center = numpy.asanyarray(center)
@@ -69,100 +78,7 @@ def find_boxed_from_particles(pos, center, subbox_size, boxsize):
         indxs.append(i)
         start_index = i + 1
 
-    return indxs
-
-
-def find_boxed(basepath, snap, pkind, fields, center, subbox_size, boxsize,
-               comm, verbose=True, dtype=numpy.float32):
-    """
-    Return particle information of particles in a box of size `subbox_size`
-    centered on `center`.
-
-    Parameters
-    ----------
-    basepath : str
-        Path to the simulation output directory.
-    snap : int
-        Snapshot number.
-    pkind : int
-        Particle type.
-    fields : list of str
-        Fields to extract.
-    center : list of float
-        Center of the sub-box.
-    subbox_size : float
-        Size of the sub-box.
-    boxsize : float
-        Size of the periodic simulation box.
-    comm : mpi4py.MPI.Comm
-        MPI communicator.
-    verbose : bool, optional
-        Print progress information.
-    dtype : type, optional
-        Data type to use for the output arrays.
-
-    Returns
-    -------
-    out : dict of n-dimensional arrays
-    """
-    if isinstance(fields, str):
-        fields = [fields]
-
-    pkind = f"PartType{pkind}"
-    files = get_snapshot_files(basepath, snap)
-
-    rank, size = comm.Get_rank(), comm.Get_size()
-
-    # Split the files list into chunks
-    chunk_size = len(files) // size
-    start = rank * chunk_size
-    end = (rank + 1) * chunk_size if rank != size - 1 else len(files)
-    nchunks = end - start
-
-    out = {f: [] for f in fields}
-
-    if verbose and rank == 0:
-        print(f"Each rank will process ~ {chunk_size} files.", flush=True)
-
-    # Only process the subset of files for this rank
-    for n, file in enumerate(files[start:end]):
-        if verbose:
-            print(f"{datetime.now()}: rank {rank} processed {n}/{nchunks} files. Currently `{file}`.",  # noqa
-                  flush=True)
-
-        with File(file, "r") as f:
-            pos = f[pkind]["Coordinates"][:].astype(dtype)
-
-            indxs = find_boxed_from_particles(pos, center, subbox_size,
-                                              boxsize)
-
-            for field in fields:
-                if field == "Coordinates":
-                    out[field].append(pos[indxs])
-                else:
-                    vals = f[pkind][field][:].astype(dtype)
-                    out[field].append(vals[indxs])
-
-    # Gather results on the master process
-    gathered_data = comm.gather(out, root=0)
-
-    del out
-    collect()
-
-    # Combine the results on the master process
-    if rank == 0:
-        combined_out = {f: [] for f in fields}
-        for data in gathered_data:
-            for field in fields:
-                combined_out[field].extend(data[field])
-
-        for field in fields:
-            combined_out[field] = numpy.concatenate(combined_out[field],
-                                                    axis=0)
-
-        return combined_out
-
-    return None
+    return pos[indxs]
 
 
 def positions_to_density_field(ngrid, pos, center, subbox_size, MAS="PCS",
